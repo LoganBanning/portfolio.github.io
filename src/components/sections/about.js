19ced857e2580e377f254fd208ae4438e6196000
LoganBanning <loganboganb@gmail.com>
2025-08-13 21:43:15 -0600

    fixed headshot

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -160,7 +160,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/2214JPEG"
+              src="../../images/headshot.JPEG"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
